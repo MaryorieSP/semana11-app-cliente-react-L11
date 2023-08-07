d60263fd260fb228e0ec30789361cf48742091e3
MaryorieSP <134321286+MaryorieSP@users.noreply.github.com>
2023-08-07 18:07:01 +0000

    Actualizando

---
+++ src/main/js/pages/nuevo-instrumento.js
@@ -0,0 +1,4 @@
+const React = require('react');
+const client = require('../client');
+const { Link} = require ('react-router-dom')
+const { useState } = require('react');
--- src/main/js/pages/nuevo-musico.js
+++ src/main/js/pages/nuevo-musico.js
@@ -1,16 +1,31 @@
 const React = require('react');
 const client = require('../client');
 const { Link} = require ('react-router-dom')
-
+const { useState } = require('react');
 
 const PageNuevoMusico = () => {
+
+    const [nombre, setNombre] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        client({
+            method: 'POST',
+            path: '/api/musicos',
+            entity: { nombre: nombre },
+            headers: { 'Content-Type': 'application/json' }
+        }).done(() => {
+            window.location = '/';
+        });
+    }
+
     return(
         <>
         <h1>Nuevo Musico</h1>
-        <form>
-            <label>Nombres</label>
-            <input type="text" name="nombre"></input>
-            <input type="submit" value="Nuevo Musico"></input>
+        <form onSubmit={handleSubmit}>
+            <label>Nombre</label>
+            <input type="text" id="nombre" name="nombre" onChange={(e)=>setNombre(e.target.value)} />
+            <input type="submit" value="Nuevo Músico" />
         </form>
         <Link to='/'>Volver</Link>
         </>
